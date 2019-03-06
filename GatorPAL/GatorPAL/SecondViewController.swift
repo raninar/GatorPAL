726f44bf84fe7382e989bc5b3f905d8c527941ef
Rani <raninar@ufl.edu>
2019-03-05 23:21:29 -0500

    added button

---
+++ GatorPAL/GatorPAL/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  GatorPAL
+//
+//  Created by Rani Mahmoudi on 3/5/19.
+//  Copyright © 2019 Rani Mahmoudi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: NSObject {
+
+}
--- GatorPAL/GatorPAL/ViewController.swift
+++ GatorPAL/GatorPAL/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func StartButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "Start", sender: self)
+    }
+    
+    
 }
 
